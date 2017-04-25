45352b4aa0cc2362e954c8a7f81da35db113852c
Timon F <dev@timonf.de>
2017-04-25 18:28:15 +0200

    added role checker to each controller

---
+++ EventListener/ControllerListener.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Twigony\Bundle\FrameworkBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpKernel\Event\KernelEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class ControllerListener
+{
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    protected $authorizationChecker;
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    protected $eventDispatcher;
+
+    public function __construct(
+        AuthorizationCheckerInterface $authorizationChecker,
+        EventDispatcherInterface $eventDispatcher
+    ) {
+        $this->authorizationChecker = $authorizationChecker;
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    public function onKernelRequest(KernelEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $options = $event->getRequest()->attributes->get('options', []);
+
+        if (!array_key_exists('roles', $options)) {
+            return;
+        }
+
+        $roles = $options['roles'];
+
+        if (is_scalar($roles)) {
+            $roles = [$roles];
+        }
+
+        if (!$this->isGrantedByRoles($roles)) {
+            throw new AccessDeniedHttpException(
+                sprintf('Current user does not have any of the required roles ("%s")', implode(', ', $roles))
+            );
+        }
+    }
+
+    protected function isGrantedByRoles($roles)
+    {
+        foreach ($roles as $role) {
+            if ($this->authorizationChecker->isGranted($role)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
